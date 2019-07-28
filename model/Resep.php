5ba3baf722daea7aa23163f24b9db096f5e6f0a2
Rizal Arif Nugraha <43497169+rizalarifnugraha@users.noreply.github.com>
2019-07-28 20:37:44 +0700

    menambahkan model resepobat

--- model/Resep.php
+++ model/Resep.php
@@ -7,7 +7,7 @@ class resep
     {
         $conn = dbConnect();
         if ($conn->connect_errno == 0) {
-            $sql = "SELECT * FROM resep";
+            $sql = "SELECT * FROM resep_dokter";
             $res = $conn->query($sql);
             if ($res) {
                 $data = $res->fetch_all(MYSQLI_ASSOC);
@@ -26,7 +26,7 @@ class resep
     {
         $conn = dbConnect();
         if ($conn->connect_errno == 0) {
-            $sql = "SELECT * FROM resep WHERE id_resep='$id_resep'";
+            $sql = "SELECT * FROM resep_dokter WHERE id_resep='$id_resep'";
             $res = $conn->query($sql);
             $data = $res->fetch_assoc();
             $row_cnt = $res->num_rows;
@@ -45,7 +45,7 @@ class resep
     {
         $conn = dbConnect();
         if ($conn->connect_errno == 0) {
-            $sql = "SELECT id_resep FROM resep ORDER BY id_resep DESC LIMIT 1";
+            $sql = "SELECT id_resep FROM resep_dokter ORDER BY id_resep DESC LIMIT 1";
             $res = $conn->query($sql);
             $data = $res->fetch_assoc();
             $row_cnt = $res->num_rows;
@@ -64,7 +64,7 @@ class resep
     {
         $conn = dbConnect();
         if ($conn->connect_errno == 0) {
-            $sql = "INSERT INTO resep(id_resep,id_dokter,id_pasien,nama_obat,jenis)
+            $sql = "INSERT INTO resep_dokter(id_resep,id_dokter,id_pasien,nama_obat,jenis)
                     VALUES('$id_resep','$id_dokter', '$id_pasien', '$nama_obat', '$jenis')";
             $res = $conn->query($sql);
             if ($res) return true; else return false;
@@ -77,7 +77,7 @@ class resep
     {
         $conn = dbConnect();
         if ($conn->connect_errno == 0) {
-            $sql = "UPDATE resep SET 
+            $sql = "UPDATE resep_dokter SET 
                                      id_dokter='$id_dokter',
                                      id_pasien='$id_pasien',
                                      nama_obat='$nama_obat',
@@ -97,7 +97,7 @@ class resep
     {
         $conn = dbConnect();
         if ($conn->connect_errno == 0) {
-            $sql = "DELETE FROM resep WHERE id_resep='$id_resep'";
+            $sql = "DELETE FROM resep_dokter WHERE id_resep='$id_resep'";
             $res = $conn->query($sql);
             if ($res) return true; else return false;
         } else {
